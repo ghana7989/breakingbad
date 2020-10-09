a570742a067950b3be66d6832d5905b1556c7687
ghana7989 <ghana.chindukuri@gmail.com>
2020-10-09 22:05:00 +0530

    adding link to href

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
 
     return (
         <div style={{ bottom: 10, left: 10, width: "inherit", backgroundColor: "#fa0", color: "#222", height: 30, display: "flex", justifyContent: "space-around", position: "fixed", alignItems: "center", border: 0, borderRadius: 5 }}>
-            <a style={{ textDecoration: "none", fontSize: 16, color: "#222", padding: 16 }} href="#">Click Here for source code</a>
+            <a style={{ textDecoration: "none", fontSize: 16, color: "#222", padding: 16 }} href="https://github.com/ghana7989/breakingbad">Click Here for source code</a>
         </div>
     )
 }
